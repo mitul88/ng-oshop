16503a89fc0931fbf987d7e86de880d837d456d4
shahed <shahed.rahman88@gmail.com>
2022-04-10 20:32:10 +0600

    firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+
 import { AppComponent } from './app.component';
+
+import { environment } from 'src/environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'ng-oshop-ffda4',
+    appId: '1:247536083245:web:45d5074c1a527847cf73a4',
+    storageBucket: 'ng-oshop-ffda4.appspot.com',
+    apiKey: 'AIzaSyCMUM1OEfMGGMaeZh5YQZYi7F0HZFhvMN8',
+    authDomain: 'ng-oshop-ffda4.firebaseapp.com',
+    messagingSenderId: '247536083245',
+  }  
 };
 
 /*
